8269258c98e59611d915b82376cc5fec835bba9a
Annabel Cheng <chen1036@algonquinlive.com>
2026-01-24 17:10:04 -0500

    Formating a new file for PA2 with a header and a signoff

---
+++ PracticalAssessment02/main.py
@@ -0,0 +1,27 @@
+"""
+Docstring for main.py of Practical Assessment 2
+
+Course: CST8002 Section 010
+Professor: Stanley Pieda
+
+Author: Annabel Cheng
+Student ID: 041146557
+
+Description: A File-IO reading / writing program  for Practical Assessment 2 - 
+             Proof of Concept. 
+
+Version: Python 3.14.2
+Date: 2026.01.24
+
+Reference: 
+1. W3Schools, "Python File Open - File Handling" W3.CSS, [Online]. 
+   Available: https://www.w3schools.com/python/python_file_handling.asp. [Accessed 24 1 2026].
+
+"""
+
+
+
+print ("-------------------------------------------------------") #Divider
+print ("1.Message: File IO program for Practical Assessment 2") #Show main message.
+print ("2.Version: Python 3.14.2")    #Show programming language used, and version.
+print ("3.Author:  Annabel Cheng")    #show author's name as seen in ACSIS
