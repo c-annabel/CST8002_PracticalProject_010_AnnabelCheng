c44955dcc45b563afbda6e2ef1adc44dc165cd95
Annabel Cheng <chen1036@algonquinlive.com>
2026-01-24 16:09:15 -0500

    Add initial Python script Hello Tuna Fish for PA01

---
+++ HelloTunaFish.py
@@ -0,0 +1,24 @@
+"""
+Docstring for HelloTunaFish
+
+Course: CST8002 Section 010
+Professor: Stanley Pieda
+
+Author: Annabel Cheng
+Student ID: 041146557
+
+Description: A modified Hello World program for Practical Assessment 1 - 
+             Proof of Concept. 
+
+Version: Python 3.14.2
+Date: 2026.01.17
+
+Reference: 
+1. W3Schools, "Python Output / Print," W3.CSS, [Online]. 
+   Available: https://www.w3schools.com/python/python_output.asp. [Accessed 17 1 2026].
+
+"""
+
+print ("1.Message: Hello Tuna Fish!") #Show main message.
+print ("2.Version: Python 3.14.2")    #Show programming language used, and version.
+print ("3.Author:  Annabel Cheng")    #show author's name as seen in ACSIS
