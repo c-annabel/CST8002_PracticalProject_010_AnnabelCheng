8b4785676e255c0f7bcdb87239514f26b86fb883
Annabel Cheng <chen1036@algonquinlive.com>
2026-01-24 19:09:22 -0500

    Established a file structure for both menu and file-io programs.

---
+++ PracticalAssessment02/menu/main.py
@@ -0,0 +1,33 @@
+"""
+Docstring for main.py of Practical Assessment 2
+
+Course: CST8002 Section 010
+Professor: Stanley Pieda
+
+Author: Annabel Cheng
+Student ID: 041146557
+
+Description: Provides a UI with a menu system for Practical Assessment 2 :
+             displaying options to read/write files and calling file_manager.py
+             to perform the actual file-io operations. 
+
+Version: Python 3.14.2
+Date: 2026.01.24
+
+Reference: 
+[1] 	W3Schools, "Python File Open," W3.CSS, [Online]. 
+      Available: https://www.w3schools.com/python/python_file_handling.asp. [Accessed 24 1 2026].
+[2] 	P. S. Foundation, "Input and Output," Python Software Foundation, [Online]. 
+      Available: https://docs.python.org/3/tutorial/inputoutput.html. [Accessed 24 1 2026].
+[3] 	J. Mertz, "Reading and Writing Files in Python (Guide)," DevCademy Media Inc. , [Online]. 
+      Available: https://realpython.com/read-write-files-python/. [Accessed 24 1 2026].
+
+"""
+
+
+
+
+print ("-------------------------------------------------------") #Divider
+print ("Message: File-IO program for Practical Assessment 2") #Show main message.
+print ("Version: Python 3.14.2")    #Show programming language used, and version.
+print ("Author:  Annabel Cheng")    #show author's name as seen in ACSIS
