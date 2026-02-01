8ce6d5bc408fc87b3af43eae2683170d77b19878
Annabel Cheng <chen1036@algonquinlive.com>
2026-02-01 09:58:25 -0500

    Created Practical Project folder and it's main.py

---
+++ SelfLearningPractices/01_CheckEditorVersion.py
@@ -0,0 +1,12 @@
+import sys
+
+#Check the Python version of the Editor
+print(sys.version)
+
+# The Python Running Command:  
+# C:\>python --versin    #Check version
+# C:\>python filename.py #Execute Python syntax file
+# C:\>python
+# >>> (codes)
+# (Run result)
+# exit() #Exit command line interface
---
+++ SelfLearningPractices/02_EntryNotes.py
@@ -0,0 +1,5 @@
+#print multiple words on the same line, you can use the end parameter:
+print("Hello World!", 88, end=" ")
+print("I will print on the same line.")
+
+#Variables do not need to be declared with any particular type,
