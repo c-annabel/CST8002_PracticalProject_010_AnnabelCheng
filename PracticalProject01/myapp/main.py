a82a7023ddd6f128e2d75e59fa67eef0441fd6ae
Annabel Cheng <chen1036@algonquinlive.com>
2026-02-01 13:44:17 -0500

    Updating comments and function names

--- PracticalProject01/myapp/main.py
+++ PracticalProject01/myapp/main.py
@@ -1,5 +1,5 @@
 """
-Docstring for main.py of Practical Assessment 2
+Docstring for main.py of Practical Project 1
 
 Course: CST8002 Section 010
 Professor: Stanley Pieda
@@ -7,9 +7,8 @@ Professor: Stanley Pieda
 Author: Annabel Cheng
 Student ID: 041146557
 
-Description: Provides a UI with a menu system for Practical Assessment 2 :
-             displaying options to read/write files and calling file_manager.py
-             to perform the actual file-io operations. 
+Description: This program applies fundamental concepts such as variables, methods, loops, file I/O, 
+             exception handling, libraries, and array-like data structures.
 
 Version: Python 3.14.2
 Date: 2026.02.01
@@ -30,10 +29,10 @@ Reference:
 from file_manager.FileManager import FileManager
 
 
-def menu():
-   """Displays a looped menu for file i/o operations."""
+def main():
+   """Displays a looped main for file i/o operations."""
 
-   # Store user name to display in the menu
+   # Store user name to display in the main
    user_name = "Annabel Cheng"
    filename = "pacific_rim_npr_coastalmarine_migratory_shorebird_habitat_use_2011-2017_data.csv"
 
@@ -43,15 +42,15 @@ def menu():
    # Greet the user once at the start
    print(f"\nWelcome, {user_name}!")
 
-   # Start the main menu loop
+   # Start the main loop
    while True: 
-      # Display menu options
-      print("\n------ File I/O Menu ------")
+      # Display main options
+      print("\n------ File I/O main ------")
       print("1. Read a file")
       print("2. Write a file")
       print("3. Exit")
       print("---------------------------\n")
-      # Ask user for menu choice
+      # Ask user for main choice
       choice = input("Enter your choice (1-3): ").strip()
 
       # Option 1: Read a file
@@ -77,13 +76,13 @@ def menu():
       else: 
          print("Invalid choice. Please select 1, 2, or 3.")
     
-# Run the menu function only if this script is executed directly
+# Run the main function only if this script is executed directly
 if __name__ == "__main__":
-   menu()
+   main()
 
 # signoff info
-print ("-------------------------------------------------------") #Divider
-print ("Message: File-IO program for Practical Project 1") #Show main message.
+print ("===========================================================") #Divider
+print ("Message: Fundamental Python program for Practical Project 1") #Show main message.
 print ("Version: Python 3.14.2")    #Show programming language used, and version.
 print ("Author:  Annabel Cheng")    #show author's name as seen in ACSIS
-print ("-------------------------------------------------------\n") #Dividera
+print ("===========================================================\n") #Divider
